b823d388f3c92ce08265b350fb66f5af00d4a99e
BrendanMolloy <brendanmolloyonline@gmail.com>
2019-10-17 13:30:29 +0000

    menu fully functional

---
+++ .bashrc
@@ -0,0 +1,1 @@
+export MONGO_URI="mongodb+srv://root:r00tUser@myfirstcluster-avk3w.mongodb.net/myTestDB?retryWrites=true&w=majority"
---
+++ launch.sh
@@ -0,0 +1,4 @@
+pip3 install dnspython
+pip3 install pymongo
+export MONGO_URI="mongodb+srv://root:<password>@myfirstcluster-avk3w.mongodb.net/test?retryWrites=true&w=majority"
+run mongo_project.py from window (not CLI)
